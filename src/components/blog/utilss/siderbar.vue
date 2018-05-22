3eba55f82504347abea2afda1f2c2139a4c1462a
adsionli <531020509@qq.com>
2018-05-22 19:13:51 +0800

    ljq

--- src/components/blog/utilss/siderbar.vue
+++ src/components/blog/utilss/siderbar.vue
@@ -4,6 +4,17 @@
         <div style="z-index: 1050;transform: translateX(0);color: #fff;text-align: center;" v-show="siderBarShow">
             <div style="padding-top:10px"> 
                 <img src="http://p53z0yfgy.bkt.clouddn.com/cat.gif" alt="64*64" width="150px" height="150px" style="border-radius:50%;border: 2px solid rgb(217,217,217);">
+                <p class="site-author-name">AdsionLi</p>
+                <p class="site-description">Enjoy code · Unique</p>
+                <p class="site-description">这就是我，钟情痴迷于代码并拥有着天马行空想象力的程序员</p>
+            </div>
+            <div style="padding-top:20px">
+                <span>
+                    <a href="https://coding.net/u/Adsionli">Coding</a>
+                </span>
+                <span>
+                    
+                </span>
             </div>
         </div>
     </div>
@@ -59,5 +70,17 @@ export default {
         background: #333;
         opacity: 0.5;
     }
-
+    .site-author-name{
+        margin: 5px 0 0;
+        text-align: center;
+        color: #f5f5f5;
+        font-weight: normal;
+        font-size: 20px;
+    }
+    .site-description{
+        margin-top: 5px;
+        text-align: center;
+        font-size: 14px;
+        color: #999;
+    }
 </style>
