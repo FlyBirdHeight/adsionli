9324c590ad462a48107c757e5dcb80c1cd7fef8e
adsionli <531020509@qq.com>
2018-05-30 21:58:58 +0800

    ljq

--- src/components/chat/chatRoom/chatRoomBody.vue
+++ src/components/chat/chatRoom/chatRoomBody.vue
@@ -6,11 +6,9 @@
                 123
             </div>
             <div class="showchat_input">
-                <div class="showchat_setting">
-                    123
-                </div>
-                <div>
-                    456
+                <mavon-editor v-model="value" :toolbars="toolbars" style="min-height:260px"/>
+                <div style="height:40px">
+                    <el-button style="float:right;margin-right:20px;margin-top:7px" size="mini" type="primary" @click="sendMail()">发送</el-button>
                 </div>
             </div>
         </div>
@@ -26,18 +24,30 @@ export default {
     },
     data () {
         return {
-
+            value:'',
+            toolbars: {
+                bold: true, // 粗体
+                italic: true,// 斜体
+                header: true,// 标题
+                // subfield: true, // 单双栏模式
+                // preview: true, // 预览
+                link: true, // 链接
+                imagelink: true, // 图片链接
+                code: true, // code
+            }
         }
     },
     methods: {
-
+        sendMail(){
+            console.log(this.value);
+        }
     }   
 }
 </script>
 
 <style>
 .showchat_body{
-    height: 450px;
+    height: 350px;
     overflow-y: auto;
     position: relative;
     top: 0;
@@ -47,7 +57,7 @@ export default {
     border-bottom: 1px solid rgb(217,217,217)
 }
 .showchat_input{
-
+    height: 250px;
 }
 .showchat_setting{
     height: 30px;
