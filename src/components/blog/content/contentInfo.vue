01678091fb5726dca6fc50885be8137cc82d1447
adsionli <531020509@qq.com>
2018-06-02 21:22:56 +0800

    ljq

--- src/components/blog/content/contentInfo.vue
+++ src/components/blog/content/contentInfo.vue
@@ -2,40 +2,26 @@
     <div>
         <el-container style="margin:auto;" :style="{width:maxWidth}">
             <el-header style="height:100px">
-                <h1 style="text-align:center">手动排查Linux磁盘占用</h1>
+                <h1 style="text-align:center">{{blog.title}}</h1>
                 <div style="margin-top:5px;margin-bottom:30px;text-align:center;">
                     <span style="color:#999;font-size: 12px">
                         <span>发布于</span>
-                        <time>2018-05-15</time>
+                        <time>{{blog.created_at}}</time>
                     </span>
                     <span style="margin-left:5px;margin-right:10px;">
                         <i style="border:1px solid #999"></i>
                     </span>
                     <span style="color:#999;font-size: 12px">
                         <span>分类于</span>
-                        <a href="javascript:;" @click="select()"  style="color:#999;font-size: 12px"><span>Linux</span></a>
+                         <a href="javascript:;" @click="seeTag(tag.id)"  style="color:#999;font-size: 12px" v-for="tag in blog.tag" :key="tag.id"><span>{{tag.name}}</span></a>
                     </span>
                 </div>
             </el-header>
             <el-main style="text-align:left;margin-top:20px">
-                <div class="post-body" itemprop="articleBody">
-                    <p>全面解析.gitignore文件中的模式匹配。</p>
-                    <p>.gitignore文件里的模式匹配有时真的让人很无语，看似非常简单，实则到处是坑，  本文我们来个彻底解析。本文来自git文档加上自己的理解，以及总结了一些常用实例，git文档可以参考文末的链接。</p>
-                    <h3 id="语法-pattern-format"><a href="#语法-pattern-format" class="headerlink" title="语法(pattern format)"></a>语法(pattern format)</h3><h4 id="第一部分语法规则" class="h4_1"><a href="#第一部分语法规则" class="headerlink" title="第一部分语法规则"></a>第一部分语法规则</h4><ol>
-                    <li>空行被忽略</li>
-                    <li>以<code>#</code>开头的行视为注释，需要<code>#</code>的地方要进行转意</li>
-                    <li>其余每行表示一个pattern</li>
-                    <li><strong>行尾</strong> 的空格被忽略，需要行尾空格的地方要进行转意</li>
-                    <li><code>!</code>意味着<code>取反</code>，即原本被匹配上的文件被忽略，加上<code>!</code>后，这些文件则被包含进来，需要<code>!</code>的地方要要进行转意。但是<strong>如果一个文件的parent directory被忽略，则该文件不会被包含进来</strong>。</li>
-                    <li>以<code>/</code>结尾的pattern，<code>/</code>将会被移除，然后按照<strong>下面</strong>的规则进行匹配，但是<strong>只匹配目录</strong></li>
-                    <li>如果pattern中没有<code>/</code>，则git把该pattern当作一个shell glob去匹配文件路径</li>
-                    <li>如果pattern中包含<code>/</code>，则git把该pattern当作一个<strong>受限</strong>的shell glob去匹配文件路径：此时wildcards(<code>*</code>)<strong>不能</strong>匹配<code>/</code>符号</li>
-                    <li>以<code>/</code>开头的pattern，<code>/</code>匹配文件路径的开始，如<code>/*.c</code>匹配<code>cat.c</code>而不匹配<code>mozilla/cat.c</code></li>
-                    </ol>
-                </div>
+                <vue-markdown v-highlight :source="blog.content"></vue-markdown>
             </el-main>
             <el-footer>
-                <a href="javascropt:;" @click="seeTag(1)"><el-tag size="mini">#Linux</el-tag></a>
+                <a href="javascript:;" @click="seeTag(tag.id)"  style="color:#999;font-size: 12px" v-for="tag in blog.tag" :key="tag.id"><el-tag size="mini">#{{tag.name}}</el-tag></a>
                 <div style="border-top:1px solid rgb(217,217,217);margin-top:10px;padding: 10px;white-space:nowrap;overflow: hidden;width:100%" :style="{width:maxWidth}">
                     <div style="display:inline-block;width:50%;white-space:normal">
                         <a href="javascript:;" @click="changeContent(1)" class="navItem"><i class="glyphicon glyphicon-chevron-left" />123</a>
@@ -56,38 +42,22 @@
                         style="margin-left:2%"
                         >
                     </el-input>
-                     <el-button type="primary" style="float:right;margin-top:15px">评论</el-button>
+                    <el-button type="primary" style="float:right;margin-top:15px" @click="addComment()">评论</el-button>
                 </div>
                 <div style="width:100%;margin-top:40px;padding:10px;text-align:center">
-                    <el-container style="margin-top:15px;margin-bottom:30px">
+                    <el-container style="margin-top:15px;margin-bottom:30px" v-for="comment in blog.comment" :key="comment.id">
                         <el-aside width="20%">
-                            <img src="http://p53z0yfgy.bkt.clouddn.com/cat.gif" alt="64*64" width="60px" height="60px" style="border-radius:50%;border: 2px solid rgb(217,217,217);">
+                            <img :src="comment.user.avatar" alt="64*64" width="60px" height="60px" style="border-radius:50%;border: 2px solid rgb(217,217,217);">
                         </el-aside>
                         <el-container>
                             <el-header style="text-align:left;height:30px">
-                                <a href="javascript:;"><span style="color:rgb(103,197,58);font-size:1.2em;font-weight:600">AdsionLi</span></a>
+                                <a href="javascript:;"><span style="color:rgb(103,197,58);font-size:1.2em;font-weight:600">{{comment.user.name}}</span></a>
                                 <div style="display: inline-block;margin-left: 0.5em;color: rgba(0, 0, 0, 0.4);font-size: 0.875em;font-weight:600">
-                                    2018-05-22
+                                    {{comment.created_at}}
                                 </div>
                             </el-header>
                             <el-main style="text-align:left;padding-top:0px;height:auto;border:1px solid rgb(217,217,217);border-radius:5px;background-color: #fff;box-shadow: 5px 5px 5px #f1f2f3;">
-                                123456
-                            </el-main>
-                        </el-container>
-                    </el-container>
-                    <el-container style="margin-top:15px;margin-bottom:30px">
-                        <el-aside width="20%">
-                            <img src="http://p53z0yfgy.bkt.clouddn.com/cat.gif" alt="64*64" width="60px" height="60px" style="border-radius:50%;border: 2px solid rgb(217,217,217);">
-                        </el-aside>
-                        <el-container>
-                            <el-header style="text-align:left;height:30px">
-                                <a href="javascript:;"><span style="color:rgb(103,197,58);font-size:1.2em;font-weight:600">AdsionLi</span></a>
-                                <div style="display: inline-block;margin-left: 0.5em;color: rgba(0, 0, 0, 0.4);font-size: 0.875em;font-weight:600">
-                                    2018-05-22
-                                </div>
-                            </el-header>
-                            <el-main style="text-align:left;padding-top:0px;height:auto;border:1px solid rgb(217,217,217);border-radius:5px;background-color: #fff;box-shadow: 5px 5px 5px #f1f2f3;">
-                                123456
+                                <vue-markdown v-highlight :source="comment.body"></vue-markdown>
                             </el-main>
                         </el-container>
                     </el-container>
@@ -98,18 +68,25 @@
 
 </template>
 
+
 <script>
 import Bus from '../../Bus.js'
+import VueMarkdown from 'vue-markdown';
 export default {
+    components: {
+        VueMarkdown
+    },
     data () {
         return {
             screenWidth:document.body.clientWidth,
             maxWidth:'755px',
             showTextArea:false,
-            comment:''
+            comment:'',
+            blog:[]
         }
     },
     mounted () {
+        this.blogInfo();
         if(sessionStorage.getItem('user')!=null){
             this.showTextArea = true;
         }else{
@@ -127,7 +104,6 @@ export default {
         }else{
             this.maxWidth = "755px";
         }
-        console.log(this.showTextArea)
     },
     methods: {
         seeTag(id){
@@ -142,6 +118,26 @@ export default {
         bus(){
             Bus.$on('userInfo',(e) => {
                 this.showTextArea = e;
+            })
+        },
+        blogInfo(){
+            this.axios.get('/api/v1/blog/'+this.$route.params.id).then((res) => {
+                this.blog = res.data.response;
+            }).catch((error) => {
+                console.log(error);
+            })
+        },
+        addComment(){
+            this.axios.post('/api/v1/add/comment',{
+                'blog_id':this.$route.params.id,'user_id':JSON.parse(sessionStorage.getItem('user')).id,'body':this.comment
+            }).then((res) => {
+                // console.log(res.data)
+                if(res.data.status=='success'){
+                    // console.log(res.data.response)
+                    this.blog.comment.splice(0,0,res.data.response);
+                }
+            }).catch((error) => {
+                console.log(error);
             })
         }
     },
--- src/components/blog/content/index.vue
+++ src/components/blog/content/index.vue
@@ -1,65 +1,87 @@
 <template>
     <div style="text-align:center">
-        <el-container style="margin:auto;" :style="{width:maxWidth}">
+        <el-container style="margin:auto;" :style="{width:maxWidth}" v-for="blog in blogs" :key="blog.id">
             <el-header style="height:100px">
-                <h1 style="text-align:left"><a href="javascript:;" @click="seeAll(1)" class="post-title-link">手动排查Linux磁盘占用</a></h1>
+                <h1 style="text-align:left"><a href="javascript:;" @click="seeAll(blog.id)" class="post-title-link">{{blog.title}}</a></h1>
                 <div style="margin-top:5px;margin-bottom:20px;text-align:left;">
                     <span style="color:#999;font-size: 12px">
                         <span>发布于</span>
-                        <time>2018-05-15</time>
+                        <time>{{blog.created_at}}</time>
                     </span>
                     <span style="margin-left:5px;margin-right:10px;">
                         <i style="border:1px solid #999"></i>
                     </span>
                     <span style="color:#999;font-size: 12px">
                         <span>分类于</span>
-                        <a href="javascript:;" @click="select()"  style="color:#999;font-size: 12px"><span>Linux</span></a>
+                        <a href="javascript:;" @click="select(tag.id)"  style="color:#999;font-size: 12px" v-for="tag in blog.tag" :key="tag.id"><span>{{tag.name}}</span></a>
                     </span>
                 </div>
             </el-header>
-            <el-main style="text-align:left;height:420px;overflow:hidden;text-overflow: ellipsis;margin-top:20px">
-                <div class="post-body" itemprop="articleBody">
-                    <p>全面解析.gitignore文件中的模式匹配。</p>
-                    <p>.gitignore文件里的模式匹配有时真的让人很无语，看似非常简单，实则到处是坑，  本文我们来个彻底解析。本文来自git文档加上自己的理解，以及总结了一些常用实例，git文档可以参考文末的链接。</p>
-                    <h3 id="语法-pattern-format"><a href="#语法-pattern-format" class="headerlink" title="语法(pattern format)"></a>语法(pattern format)</h3><h4 id="第一部分语法规则" class="h4_1"><a href="#第一部分语法规则" class="headerlink" title="第一部分语法规则"></a>第一部分语法规则</h4><ol>
-                    <li>空行被忽略</li>
-                    <li>以<code>#</code>开头的行视为注释，需要<code>#</code>的地方要进行转意</li>
-                    <li>其余每行表示一个pattern</li>
-                    <li><strong>行尾</strong> 的空格被忽略，需要行尾空格的地方要进行转意</li>
-                    <li><code>!</code>意味着<code>取反</code>，即原本被匹配上的文件被忽略，加上<code>!</code>后，这些文件则被包含进来，需要<code>!</code>的地方要要进行转意。但是<strong>如果一个文件的parent directory被忽略，则该文件不会被包含进来</strong>。</li>
-                    <li>以<code>/</code>结尾的pattern，<code>/</code>将会被移除，然后按照<strong>下面</strong>的规则进行匹配，但是<strong>只匹配目录</strong></li>
-                    <li>如果pattern中没有<code>/</code>，则git把该pattern当作一个shell glob去匹配文件路径</li>
-                    <li>如果pattern中包含<code>/</code>，则git把该pattern当作一个<strong>受限</strong>的shell glob去匹配文件路径：此时wildcards(<code>*</code>)<strong>不能</strong>匹配<code>/</code>符号</li>
-                    <li>以<code>/</code>开头的pattern，<code>/</code>匹配文件路径的开始，如<code>/*.c</code>匹配<code>cat.c</code>而不匹配<code>mozilla/cat.c</code></li>
-                    </ol>
-                </div>
+            <el-main style="text-align:left;max-height:420px;overflow:hidden;text-overflow: ellipsis;margin-top:20px">
+                <vue-markdown v-highlight :source="blog.content"></vue-markdown>
             </el-main>
             <el-footer style="text-align:right">
-                <a class="btn_a" href="javascript:;" @click="seeAll()">
+                <a class="btn_a" href="javascript:;" @click="seeAll(blog.id)">
                     阅读全文 »
                 </a>
             </el-footer>
         </el-container>
+        <div class="block">
+            <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="currentPage4"
+                :page-sizes="[10, 20, 30, 40]"
+                :page-size="blogSize"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="blogTotal">
+            </el-pagination>
+        </div>
     </div>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown';
 export default {
   methods:{
-      select(){
-          console.log(123456);
+      select(id){
+          console.log(id);
       },
       seeAll(id){
           this.$router.push('/blog/blogInfo/'+id);
-      }
+      },
+      blogsAll(page,size){
+          this.axios.post('/api/v1/blog',{
+              'page':page,'count':size
+          }).then((res) => {
+              if(res.data.status == 'success'){
+                  this.blogs = res.data.response.blogs;
+                  this.blogTotal = res.data.response.number;
+              }
+          }).catch((error) => {
+              console.log(error);
+          })
+      },
+      handleSizeChange(val){
+        this.blogSize = val;
+        this.blogsAll(this.currentPage4,this.blogSize);
+      },
+      handleCurrentChange(val){
+        this.blogsAll(this.currentPage4,this.blogSize);
+      },
   },
   data () {
       return {
         screenWidth:document.body.clientWidth,
-        maxWidth:'755px'
+        maxWidth:'755px',
+        blogs:[],
+        currentPage4: 1,
+        blogTotal:20,
+        blogSize:10,
       }
   },
   mounted () {
+        this.blogsAll(this.currentPage4,this.blogSize);
         const that = this;
         window.onresize = () => {
             return (() => {
@@ -71,7 +93,7 @@ export default {
             this.maxWidth = "100%";
         }else{
             this.maxWidth = "755px";
-        }
+        } 
     },
     watch: {
         screenWidth (val) {
@@ -82,6 +104,9 @@ export default {
                 this.maxWidth = "755px";
             }
         }
+    },
+    components: {
+        VueMarkdown
     }
 }
 </script>
