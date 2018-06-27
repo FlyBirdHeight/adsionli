7fa6ff6712e1cfe74089d941cceb0ad94b10ec20
adsionli <531020509@qq.com>
2018-06-27 12:53:06 +0800

    ljq

--- src/components/chat/header/sider.vue
+++ src/components/chat/header/sider.vue
@@ -19,10 +19,37 @@
             <el-dialog
                 width="25%"
                 :visible.sync="userInfoSee"
+                :title="userInfo.name"
                 :modal="false"
                 append-to-body
+                center
                 >
-
+                <div style="text-align:center">
+                    <img :src="userInfo.avatar" alt="100*100" width="100" height="100" style="border-radius:50%">
+                </div>
+                <div style="color:rgb(167,167,167);font-size:13px;text-align:center;margin-top:15px">
+                    <p>{{userInfo.description==""?"这个人有那么点点懒，没有自我介绍呢~":userInfo.description}}</p>
+                </div>
+                <div style="text-align:center;margin-top:15px;font-size:13px">
+                    <ul style="list-style-type:none;">
+                        <li style="margin-left:-40px;margin-top:10px">
+                            <span style="color:rgb(167,167,167)">昵称：<font style="color:black">{{userInfo.name}}</font></span>
+                        </li>
+                        <li style="margin-left:-40px;margin-top:10px">
+                            <span style="color:rgb(167,167,167)">邮箱：<font style="color:black">{{userInfo.email}}</font></span>
+                        </li>
+                        <li style="margin-left:-40px;margin-top:10px">
+                            <span style="color:rgb(167,167,167)">性别：<font style="color:black">{{userInfo.sex}}</font></span>
+                        </li>
+                        <li style="margin-left:-40px;margin-top:10px">
+                            <span style="color:rgb(167,167,167)">年龄：<font style="color:black">{{userInfo.age}}</font></span>
+                        </li>
+                    </ul>
+                </div>
+                <div slot="footer" >
+                    <el-button @click="setChat(userInfo.id)">发起聊天</el-button>
+                    <el-button type="primary" @click="follower(userInfo.id)">加为好友</el-button>
+                </div>
             </el-dialog>
 
             <el-tabs type="border-card" v-model="roomSeeing" @tab-click="clickRoomSeeing">
@@ -110,6 +137,12 @@ export default {
             }).catch((error) => {
                 console.log(error);
             })
+        },
+        setChat(id){
+            console.log(id);
+        },
+        follower(id){
+            console.log(id)
         }
     },
     mounted(){
